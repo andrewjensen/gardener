a2353ba412a1c597e1286c36a507d1b2a8ae9c55
Andrew Jensen <andrewjensen90@gmail.com>
2023-02-03 20:31:12 -0700

    Inline format variable

--- src/views.rs
+++ src/views.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 
 pub fn get_view_path(view_name: &str) -> PathBuf {
     let mut view_path = env::current_dir().unwrap();
-    view_path.extend(["public", "views", &format!("{}.html", view_name)].iter());
+    view_path.extend(["public", "views", &format!("{view_name}.html")].iter());
 
     view_path
 }
