b2affbd7351790825bbdf214bcb5ed96b762f5ff
Andrew Jensen <andrewjensen90@gmail.com>
2023-02-06 20:55:42 -0700

    Move compiled binary into workspace, delete compilation dir

--- src/compilation_worker.rs
+++ src/compilation_worker.rs
@@ -1,5 +1,6 @@
 use log::{debug, error, info};
 use std::collections::{HashMap, VecDeque};
+use std::path::PathBuf;
 use std::process::Stdio;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -95,9 +96,9 @@ async fn compile_patch(patch_id: &str) {
 
     compile_binary(patch_id, &env_config).await;
 
-    // Step 3: copy binary into workspace
+    move_binary_into_workspace(patch_id, &env_config).await;
 
-    // Step 4: remove compilation dir
+    remove_build_dir(patch_id, &env_config).await;
 }
 
 async fn generate_cpp_code(patch_id: &str, env_config: &EnvConfig) {
@@ -131,12 +132,10 @@ async fn generate_cpp_code(patch_id: &str, env_config: &EnvConfig) {
 }
 
 async fn compile_binary(patch_id: &str, env_config: &EnvConfig) {
-    let mut dir_patch_build = env_config.dir_pd2dsy.clone();
-    dir_patch_build.push("builds");
-    dir_patch_build.push(patch_id);
+    let dir_patch_build = get_dir_patch_build(patch_id, env_config);
 
     let mut child = Command::new("make")
-        .current_dir(dir_patch_build.as_path())
+        .current_dir(dir_patch_build)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
@@ -146,4 +145,54 @@ async fn compile_binary(patch_id: &str, env_config: &EnvConfig) {
     if !status_code.success() {
         panic!("TODO: handle case when make fails");
     }
+}
+
+async fn move_binary_into_workspace(patch_id: &str, env_config: &EnvConfig) {
+    let dir_patch_build = get_dir_patch_build(patch_id, env_config);
+
+    let mut filename_compiled_binary = dir_patch_build.to_path_buf();
+    filename_compiled_binary.push("build");
+    filename_compiled_binary.push(format!("HeavyDaisy_{}.bin", patch_id.replace('-', "_")));
+
+    let mut filename_in_downloads = env_config.dir_workspace.clone();
+    filename_in_downloads.push("downloads");
+    filename_in_downloads.push(format!("daisy-{patch_id}.bin"));
+
+    let mut child = Command::new("mv")
+        .arg(filename_compiled_binary.as_path())
+        .arg(filename_in_downloads.as_path())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("failed to spawn");
+    let status_code = child.wait().await.unwrap();
+
+    if !status_code.success() {
+        panic!("TODO: handle case when mv fails");
+    }
+}
+
+async fn remove_build_dir(patch_id: &str, env_config: &EnvConfig) {
+    let dir_patch_build = get_dir_patch_build(patch_id, env_config);
+
+    let mut child = Command::new("rm")
+        .arg("-rf")
+        .arg(dir_patch_build.as_path())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("failed to spawn");
+    let status_code = child.wait().await.unwrap();
+
+    if !status_code.success() {
+        panic!("TODO: handle case when rm fails");
+    }
+}
+
+fn get_dir_patch_build(patch_id: &str, env_config: &EnvConfig) -> PathBuf {
+    let mut dir_patch_build = env_config.dir_pd2dsy.clone();
+    dir_patch_build.push("builds");
+    dir_patch_build.push(patch_id);
+
+    dir_patch_build
 }
