beabce02fd98e5cf43229c64891fe5eefe69f844
Andrew Jensen <andrewjensen90@gmail.com>
2023-04-18 20:56:46 -0600

    Add UI option for seed with custom JSON

---
+++ public/static/home.js
@@ -0,0 +1,18 @@
+main();
+
+async function main() {
+  console.log('main()');
+  document.querySelector('[name="board"]').addEventListener('change', (event) => {
+    onSelectBoard(event.target.value);
+  })
+}
+
+function onSelectBoard(boardValue) {
+  console.log('onSelectBoard', boardValue);
+
+  if (boardValue === 'seed_custom_json') {
+    document.getElementById('board-def-container').classList.remove('hidden');
+  } else {
+    document.getElementById('board-def-container').classList.add('hidden');
+  }
+}
